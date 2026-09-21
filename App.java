d7bdbb02c56e961ad87ea9ea327f5a39e0106914
Rashidul220 <amk1021514@student.hamk.fi>
2026-09-21 12:41:36 -0800

    Create App.java
    Add basic printer calculator

---
+++ App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String Creator = "Rashidul";
+        int number1,number2;
+         number1 =5;
+          number2 =3;
+          int multiplication,addition,subtraction;
+          Double division;
+          
+          multiplication =number1*number2;
+          addition =number1+number2;
+          division =(double)number1/number2;
+          subtraction =number1-number2;
+
+        System.out.println("Printer App");
+        System.out.println("This app made by " + Creator);
+      // System.out.println("number1");
+      System.out.println("the value of number1 is " + number1);
+      System.out.println("the value of number2 is "+ number2);
+      System.out.println("multiplication =" +multiplication);
+      System.out.println("addition ="+addition);
+      System.out.println("division ="+division);
+      System.out.println("subtraction ="+subtraction);
+
+    }
+}
+;
